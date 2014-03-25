63485efcdd017fa1d6bfee48a42fc117ec41063e
uosiu <stanislaw.skonieczny@gmail.com>
2014-03-25 13:01:51 +0100

    setup.py fixed

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ root = os.path.abspath(os.path.dirname(__file__))
 setup(
     name='modulr',
     version='0.0.1.dev',
-    description='Modular application base. ',
+    description='Modular, plugginable application base. ',
     author='Stanislaw Skonieczny',
     author_email='stanislaw.skonieczny@gmail.com',
-    url='http://github.com/TODO',
+    url='https://github.com/stanislaw-skonieczny/python-modulr',
     packages=['src/modulr'],
     install_requires=open(os.path.join(root, 'requirements.txt')).readlines(),
     test_suite='tests',
