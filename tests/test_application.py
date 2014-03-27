32e864c8dfdf4acc934c112c0ccc9f7559cde85a
uosiu <stanislaw.skonieczny@gmail.com>
2014-03-27 14:36:19 +0100

    Helpers fixed

--- tests/test_application.py
+++ tests/test_application.py
@@ -2,7 +2,7 @@ import unittest
 from mock import sentinel, Mock
 from modulr.application import ComponentFactoryRegistry, Application, simple_component_factory,\
     WrongConfigurationError, ConfiguredComponentFactory
-
+from .helpers import SimilarTo
 
 _sentinel = object()
 
@@ -14,18 +14,6 @@ class Component(object):
     
     def init(self):
         self.inited += 1
-
-
-class SimilarTo(object):
-
-    def __init__(self, **kwargs):
-        self._attrs = kwargs
-    
-    def __eq__(self, obj):
-        for attr_name, attr_value in self._attrs.items():
-            if attr_value != getattr(obj, attr_name, _sentinel):
-                return False
-        return True
 
 
 class ApplicationTest(unittest.TestCase):
